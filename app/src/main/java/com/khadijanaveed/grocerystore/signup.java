e0ffaef05d772fc4b1c993c60dfc2181bd79b6fb
ayeshahanif1 <ayeshahanif.pucit@gmail.com>
2021-06-19 22:04:59 +0500

    my initial project commit

---
+++ app/src/main/java/com/khadijanaveed/grocerystore/signup.java
@@ -0,0 +1,22 @@
+package com.khadijanaveed.grocerystore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(signup.this,login.class);
+        startActivity(intent);
+
+    }
+}
